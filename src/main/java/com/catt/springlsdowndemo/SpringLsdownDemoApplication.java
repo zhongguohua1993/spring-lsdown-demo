138b1ea6528d0db805443a1b42585bebdfdafdef
hua <m15622201762@163.com>
2019-05-28 10:32:03 +0800

    Initial commit

---
+++ src/main/java/com/catt/springlsdowndemo/SpringLsdownDemoApplication.java
@@ -0,0 +1,13 @@
+package com.catt.springlsdowndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLsdownDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringLsdownDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/catt/springlsdowndemo/SpringLsdownDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.catt.springlsdowndemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLsdownDemoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
